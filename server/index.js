148fef5a4080567efd2125bd52a796aa3cd899cb
smfahad19 <syedfahad305171@gmail.com>
2026-06-21 22:13:23 +0500

    did some changes for development

--- server/index.js
+++ server/index.js
@@ -44,8 +44,12 @@ app.use(
       }
     },
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
   })
 )
+
+app.options('*', cors())
 
 app.use(express.json())
 app.use(cookieParser())
