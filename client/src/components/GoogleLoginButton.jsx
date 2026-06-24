6aa01ea0e92e59b2dbb1b454020415c56c041c1d
smfahad19 <syedfahad305171@gmail.com>
2026-06-24 21:24:49 +0500

    fix: prisma singleton connection fix

--- client/src/components/GoogleLoginButton.jsx
+++ client/src/components/GoogleLoginButton.jsx
@@ -1,41 +1,68 @@
 import { useEffect, useRef } from 'react';
 
-const GoogleLoginButton = ({ onSuccess}) => {
+const CLIENT_ID = '1063472421493-dsbk7mc5tiuml1btm6hqbi8l4e8kps4g.apps.googleusercontent.com';
+
+const GoogleLoginButton = ({ onSuccess, onError }) => {
   const buttonRef = useRef();
 
   useEffect(() => {
-    // Load Google SDK
-    const script = document.createElement('script');
-    script.src = 'https://accounts.google.com/gsi/client';
-    script.async = true;
-    script.defer = true;
-    script.onload = () => {
-      if (window.google) {
-        window.google.accounts.id.initialize({
-          client_id: '1063472421493-dsbk7mc5tiuml1btm6hqbi8l4e8kps4g.apps.googleusercontent.com',
-          callback: (response) => {
-            // response.credential is the ID token
+    const initializeGoogle = () => {
+      if (!window.google) return;
+
+      window.google.accounts.id.initialize({
+        client_id: CLIENT_ID,
+        callback: (response) => {
+          if (response.credential) {
             onSuccess(response.credential);
-          },
-          auto_select: false,
-          cancel_on_tap_outside: true,
-        });
-        // Render the Google button inside the div
+          } else {
+            onError?.();
+          }
+        },
+        auto_select: false,
+        cancel_on_tap_outside: true,
+      });
+
+      if (buttonRef.current) {
         window.google.accounts.id.renderButton(buttonRef.current, {
           type: 'standard',
           theme: 'outline',
           size: 'large',
-          width: '380',
+          width: buttonRef.current.offsetWidth || 380,
           text: 'signin_with',
+          shape: 'rectangular',
         });
       }
     };
+
+    // Already loaded check
+    if (window.google) {
+      initializeGoogle();
+      return;
+    }
+
+    // Script already in DOM check
+    if (document.querySelector('script[src="https://accounts.google.com/gsi/client"]')) {
+      const interval = setInterval(() => {
+        if (window.google) {
+          clearInterval(interval);
+          initializeGoogle();
+        }
+      }, 100);
+      return () => clearInterval(interval);
+    }
+
+    const script = document.createElement('script');
+    script.src = 'https://accounts.google.com/gsi/client';
+    script.async = true;
+    script.defer = true;
+    script.onload = initializeGoogle;
+    script.onerror = () => onError?.();
     document.head.appendChild(script);
-  }, [onSuccess]);
+  }, []);
 
   return (
-    <div className="w-full">
-      <div ref={buttonRef} className="flex justify-center"></div>
+    <div className='w-full flex justify-center'>
+      <div ref={buttonRef} className='w-full' />
     </div>
   );
 };
